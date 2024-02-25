d1cec6289c8f09a22733dbe4ee7c3bacaa5172e1
Arturo Rivera <arturo.riverar97@gmail.com>
2024-02-24 22:31:59 -0600

    Create _Categorie widget to improve structure

--- client/lib/presentation/widgets/shared/categories.dart
+++ client/lib/presentation/widgets/shared/categories.dart
@@ -28,41 +28,59 @@ class Categories extends StatelessWidget {
               itemCount: categories.length,
               itemBuilder: (context, index) {
                 final category = categories[index];
-                return GestureDetector(
-                  onTap: () {
-                    updateProducts(category);
-                  },
-                  child: Padding(
-                    padding:
-                        const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                    child: Chip(
-                      label: Text(
-                        category,
-                        style: TextStyle(
-                            color: selectedCategory == category
-                                ? Colors.white
-                                : Colors.black),
-                      ),
-                      backgroundColor: selectedCategory == category
-                          ? Theme.of(context).colorScheme.primary
-                          : Colors.transparent,
-                      side: BorderSide(
-                        width: 1.5,
-                        color: selectedCategory == category
-                            ? Theme.of(context).colorScheme.primary
-                            : Colors.black,
-                      ),
-                      shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(9),
-                        ),
-                      ),
-                    ),
-                  ),
-                );
+                return _Categorie(
+                    updateProducts: updateProducts,
+                    category: category,
+                    selectedCategory: selectedCategory);
               }),
         )
       ],
+    );
+  }
+}
+
+class _Categorie extends StatelessWidget {
+  const _Categorie({
+    required this.updateProducts,
+    required this.category,
+    required this.selectedCategory,
+  });
+
+  final void Function(String p1) updateProducts;
+  final String category;
+  final String selectedCategory;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        updateProducts(category);
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+        child: Chip(
+          label: Text(
+            category,
+            style: TextStyle(
+                color:
+                    selectedCategory == category ? Colors.white : Colors.black),
+          ),
+          backgroundColor: selectedCategory == category
+              ? Theme.of(context).colorScheme.primary
+              : Colors.transparent,
+          side: BorderSide(
+            width: 1.5,
+            color: selectedCategory == category
+                ? Theme.of(context).colorScheme.primary
+                : Colors.black,
+          ),
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(9),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
