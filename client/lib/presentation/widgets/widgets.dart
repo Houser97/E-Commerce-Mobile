05541aa828c266da56a4e664493273b13790fe54
Arturo Rivera <arturo.riverar97@gmail.com>
2024-03-01 20:17:25 -0600

    Create file to export widgets

---
+++ client/lib/presentation/widgets/widgets.dart
@@ -0,0 +1,8 @@
+export 'package:flutter_version/presentation/widgets/cart/cart_product_cart.dart';
+export 'package:flutter_version/presentation/widgets/cart/cart_quantity_buttons.dart';
+export 'package:flutter_version/presentation/widgets/home/home_hero_card.dart';
+export 'package:flutter_version/presentation/widgets/home/home_product_card.dart';
+export 'package:flutter_version/presentation/widgets/home/product_list.dart';
+export 'package:flutter_version/presentation/widgets/shared/categories.dart';
+export 'package:flutter_version/presentation/widgets/shared/custom_button.dart';
+export 'package:flutter_version/presentation/widgets/shared/search.dart';
