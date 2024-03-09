3553ac795b9e935ba06f23a20d844dd905ff7f22
Arturo Rivera <arturo.riverar97@gmail.com>
2024-03-08 21:40:59 -0600

    Rename datasource and repository files

--- client/lib/infrastructure/repositories/local_product_repository_impl.dart
+++ client/lib/infrastructure/repositories/local_product_repository_impl.dart
@@ -2,9 +2,9 @@ import 'package:flutter_version/domain/datasources/local_product_datasource.dart
 import 'package:flutter_version/domain/entities/product.dart';
 import 'package:flutter_version/domain/repositories/local_product_repository.dart';
 
-class ProductRepositoryImpl extends LocalProductRepository {
+class LocalProductRepositoryImpl extends LocalProductRepository {
   final LocalProductDatasource datasource;
-  ProductRepositoryImpl({required this.datasource});
+  LocalProductRepositoryImpl({required this.datasource});
   @override
   List<Product> getAllLocalProducts() {
     return datasource.getAllLocalProducts();
