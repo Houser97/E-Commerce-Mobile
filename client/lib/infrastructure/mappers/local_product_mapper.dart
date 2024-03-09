8cda6ac614c12ef57de580cac729662f1f8c58af
Arturo Rivera <arturo.riverar97@gmail.com>
2024-03-08 21:38:05 -0600

    Create local product mapper

---
+++ client/lib/infrastructure/mappers/local_product_mapper.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_version/domain/entities/product.dart';
+import 'package:flutter_version/infrastructure/models/local/local_product.dart';
+
+class ProductMapper {
+  static Product productLocalToEntity(LocalProduct localProduct) => Product(
+        id: localProduct.id,
+        title: localProduct.title,
+        price: localProduct.price,
+        image: localProduct.image,
+        categories: localProduct.categories,
+        rating: localProduct.rating,
+      );
+}
