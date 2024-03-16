53e9058e2cfc4da53dd2784be671d0d68d306047
Arturo Rivera <arturo.riverar97@gmail.com>
2024-03-15 22:46:21 -0600

    Define method to get products by category

--- client/lib/domain/repositories/product_repository.dart
+++ client/lib/domain/repositories/product_repository.dart
@@ -2,4 +2,5 @@ import 'package:flutter_version/domain/entities/product.dart';
 
 abstract class LocalProductRepository {
   List<Product> getAllLocalProducts();
+  List<Product> getProductsByCategory(String category);
 }
