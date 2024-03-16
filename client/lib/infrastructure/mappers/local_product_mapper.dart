25034130780957a0fa1a37f9eb2705bde98d9750
Arturo Rivera <arturo.riverar97@gmail.com>
2024-03-15 22:54:37 -0600

    Rename method to convert local products to product

--- client/lib/infrastructure/mappers/local_product_mapper.dart
+++ client/lib/infrastructure/mappers/local_product_mapper.dart
@@ -2,7 +2,7 @@ import 'package:flutter_version/domain/entities/product.dart';
 import 'package:flutter_version/infrastructure/models/local/local_product.dart';
 
 class ProductMapper {
-  static Product productLocalToEntity(LocalProduct localProduct) => Product(
+  static Product localProductToEntity(LocalProduct localProduct) => Product(
         id: localProduct.id,
         title: localProduct.title,
         price: localProduct.price,
