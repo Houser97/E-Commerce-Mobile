f9a2e1560f98cc45c43655458316eb8b70694a5e
Arturo Rivera <arturo.riverar97@gmail.com>
2024-03-16 19:06:35 -0600

    Adjust method to convert local products to Product

--- client/lib/infrastructure/datasourcers/local_product_datasource.dart
+++ client/lib/infrastructure/datasourcers/local_product_datasource.dart
@@ -1,14 +1,11 @@
 import 'package:flutter_version/domain/datasources/product_datasource.dart';
 import 'package:flutter_version/domain/entities/product.dart';
 import 'package:flutter_version/infrastructure/mappers/local_product_mapper.dart';
-import 'package:flutter_version/infrastructure/models/local/local_product.dart';
 import 'package:flutter_version/shared/data/local_products.dart';
 
 class LocalProductDatasource extends ProductDatasource {
   List<Product> _jsonToProducts(json) {
-    final List<LocalProduct> localProducts = products.map((product) => LocalProduct.fromJson(product)).toList();
-
-    return localProducts.map((localProduct) => ProductMapper.productLocalToEntity(localProduct)).toList();
+    return json.map((localProduct) => ProductMapper.localProductToEntity(localProduct)).toList();
   }
 
   @override
