a9dd5ba6891d8c0d68b2ab6da6cc7b7d728c922a
Arturo Rivera <arturo.riverar97@gmail.com>
2024-03-17 22:54:18 -0600

    Use Future type and async functions

--- client/lib/infrastructure/datasourcers/local_product_datasource.dart
+++ client/lib/infrastructure/datasourcers/local_product_datasource.dart
@@ -4,18 +4,21 @@ import 'package:flutter_version/infrastructure/mappers/local_product_mapper.dart
 import 'package:flutter_version/shared/data/local_products.dart';
 
 class LocalProductDatasource extends ProductDatasource {
-  List<Product> _jsonToProducts(json) {
-    return json.map((localProduct) => ProductMapper.localProductToEntity(localProduct)).toList();
+  List<Product> _jsonToProducts(List<Map<String, dynamic>> json) {
+    final List<Product> products = json.map((localProduct) => ProductMapper.localProductToEntity(localProduct)).toList();
+    return products;
   }
 
   @override
-  List<Product> getAllProducts() {
+  Future<List<Product>> getAllProducts({int page = 1, int limit = 20}) async {
+    await Future.delayed(const Duration(milliseconds: 200));
     return _jsonToProducts(products);
   }
 
   @override
-  List<Product> getProductsByCategory(String category) {
+  Future<List<Product>> getProductsByCategory(String category, {int page = 1, int limit = 20}) async {
     if (category == 'All') return getAllProducts();
+    await Future.delayed(const Duration(milliseconds: 200));
     return _jsonToProducts(products).where((product) => product.categories.contains(category)).toList();
   }
 }
