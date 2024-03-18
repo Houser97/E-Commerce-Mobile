96011d80a2327659d51f2c89cb2413720d83eff5
Arturo Rivera <arturo.riverar97@gmail.com>
2024-03-17 22:56:58 -0600

    Create callbacks and provider to retrieve all products

--- client/lib/presentation/providers/products/products_providers.dart
+++ client/lib/presentation/providers/products/products_providers.dart
@@ -2,23 +2,39 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_version/domain/entities/product.dart';
 import 'package:flutter_version/presentation/providers/products/product_repository_provider.dart';
 
-final allProductsProvider = Provider<List<Product>>((ref) => ref.watch(productRepositoryProvider).getAllLocalProducts());
-
-final productsByCategoryProvider = StateNotifierProvider<ProductsByCategoryNotifier, List<Product>>((ref) {
-  final searchByCategory = ref.watch(productRepositoryProvider).getProductsByCategory;
-  return ProductsByCategoryNotifier(searchByCategory);
+final allProductsProvider = StateNotifierProvider<ProductsNotifier, List<Product>>((ref) {
+  final getAllProducts = ref.watch(productRepositoryProvider).getAllLocalProducts;
+  return ProductsNotifier(getAllProducts: getAllProducts);
 });
 
-typedef SearchByCategoryCallback = List<Product> Function(String category);
+final productsByCategoryProvider = StateNotifierProvider<ProductsNotifier, List<Product>>((ref) {
+  final searchByCategory = ref.watch(productRepositoryProvider).getProductsByCategory;
+  return ProductsNotifier(getProductsByCategory: searchByCategory);
+});
 
-class ProductsByCategoryNotifier extends StateNotifier<List<Product>> {
-  SearchByCategoryCallback searchByCategory;
-  ProductsByCategoryNotifier(this.searchByCategory) : super([]);
+typedef SearchByCallback = Future<List<Product>> Function(String category);
+typedef GetAllCallback = Future<List<Product>> Function();
 
-  Future<void> searchProductByCategory(String category) async {
-    final products = searchByCategory(category);
+class ProductsNotifier extends StateNotifier<List<Product>> {
+  final SearchByCallback? getProductsByCategory;
+  final GetAllCallback? getAllProducts;
+  ProductsNotifier({this.getProductsByCategory, this.getAllProducts}) : super([]);
+
+  Future<List<Product>> searchProductByCategory(String category) async {
+    if (getProductsByCategory == null) return [];
+    final products = await getProductsByCategory!(category);
     // Esperar a que el árbol de Widgets se renderice antes de modificar este estado.
     await Future.delayed(const Duration(milliseconds: 400));
     state = products;
+    return products;
+  }
+
+  Future<List<Product>> getProducts() async {
+    if (getAllProducts == null) return [];
+    final products = await getAllProducts!();
+    // Esperar a que el árbol de Widgets se renderice antes de modificar este estado.
+    await Future.delayed(const Duration(milliseconds: 400));
+    state = products;
+    return products;
   }
 }
