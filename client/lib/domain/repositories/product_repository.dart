d7aa12aa72f1edf3c275ada0752e211d3188255e
Arturo Rivera <arturo.riverar97@gmail.com>
2024-03-19 20:00:41 -0600

    Create single method to recover products

--- client/lib/domain/repositories/product_repository.dart
+++ client/lib/domain/repositories/product_repository.dart
@@ -1,6 +1,5 @@
 import 'package:flutter_version/domain/entities/product.dart';
 
 abstract class LocalProductRepository {
-  Future<List<Product>> getAllLocalProducts({int page = 1, int limit = 20});
-  Future<List<Product>> getProductsByCategory(String category, {int page = 1, int limit = 20});
+  Future<List<Product>> getProducts(String category, String? query, {int page = 1, int limit = 20});
 }
