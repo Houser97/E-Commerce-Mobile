10db65869f73e8c788ed258d9c376cd22a5a622a
Arturo Rivera <arturo.riverar97@gmail.com>
2024-03-22 22:01:19 -0600

    Implement getProducts in repository

--- client/lib/infrastructure/repositories/product_repository_impl.dart
+++ client/lib/infrastructure/repositories/product_repository_impl.dart
@@ -5,13 +5,9 @@ import 'package:flutter_version/domain/repositories/product_repository.dart';
 class ProductRepositoryImpl extends LocalProductRepository {
   final ProductDatasource datasource;
   ProductRepositoryImpl({required this.datasource});
-  @override
-  Future<List<Product>> getAllLocalProducts({int page = 1, int limit = 20}) {
-    return datasource.getAllProducts(limit: limit);
-  }
 
   @override
-  Future<List<Product>> getProductsByCategory(String category, {int page = 1, int limit = 20}) {
-    return datasource.getProductsByCategory(category, limit: limit);
+  Future<List<Product>> getProducts(String category, String? query, {int page = 1, int limit = 20}) {
+    return datasource.getProducts(category, query ?? '');
   }
 }
