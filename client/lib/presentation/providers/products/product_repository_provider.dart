7ee34fadba50d23d137a2352e1bd9c735305d672
Arturo Rivera <arturo.riverar97@gmail.com>
2024-03-24 19:25:52 -0600

    Import datasources from barrel file

--- client/lib/presentation/providers/products/product_repository_provider.dart
+++ client/lib/presentation/providers/products/product_repository_provider.dart
@@ -1,8 +1,9 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_version/infrastructure/datasourcers/local_product_datasource.dart';
+import 'package:flutter_version/infrastructure/datasourcers/datasources.dart';
 import 'package:flutter_version/infrastructure/repositories/product_repository_impl.dart';
 
 final productRepositoryProvider = Provider((ref) {
-  final datasource = LocalProductDatasource();
+  //final datasource = LocalProductDatasource();
+  final datasource = DummyProductDatasource();
   return ProductRepositoryImpl(datasource: datasource);
 });
