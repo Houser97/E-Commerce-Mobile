34b5dc0b6bcdc03353ffda81687e54b35ceccf5a
Arturo Rivera <arturo.riverar97@gmail.com>
2024-03-24 19:24:36 -0600

    Create datasourcers barrel file

---
+++ client/lib/infrastructure/datasourcers/datasources.dart
@@ -0,0 +1,2 @@
+export 'package:flutter_version/infrastructure/datasourcers/dummy_product_datasource.dart';
+export 'package:flutter_version/infrastructure/datasourcers/local_product_datasource.dart';
