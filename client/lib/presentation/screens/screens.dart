10de7f69350c0f3456ba08610ab3fd55d25e4360
Arturo Rivera <arturo.riverar97@gmail.com>
2024-04-02 21:43:14 -0600

    Fix ProductScreen path

--- client/lib/presentation/screens/screens.dart
+++ client/lib/presentation/screens/screens.dart
@@ -1,3 +1,3 @@
 export 'package:flutter_version/presentation/screens/cart/cart_screen.dart';
 export 'package:flutter_version/presentation/screens/home/home_screen.dart';
-export 'package:flutter_version/presentation/screens/products/product_details_screen.dart';
+export 'package:flutter_version/presentation/screens/products/product_screen.dart';
