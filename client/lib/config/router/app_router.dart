1ea860776cef1401c3b8618aff88a3a6f9aa32fc
Arturo Rivera <arturo.riverar97@gmail.com>
2024-04-12 22:18:09 -0600

    Add ProductScreen to router

--- client/lib/config/router/app_router.dart
+++ client/lib/config/router/app_router.dart
@@ -1,9 +1,13 @@
-import 'package:flutter_version/presentation/screens/home/home_screen.dart';
+import 'package:flutter_version/presentation/screens/screens.dart';
 import 'package:go_router/go_router.dart';
 
 final appRouter = GoRouter(routes: [
   GoRoute(
     path: '/',
     builder: (context, state) => const HomePage(),
+  ),
+  GoRoute(
+    path: '/product/:id',
+    builder: (context, state) => const ProductScreen(product: product),
   )
 ]);
